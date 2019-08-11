721524d6c60cd0caab1eee7a3e502c4e1d05b0a6
Cat <jarolika30@gmail.com>
2019-08-11 20:06:03 +0300

    feat: add ability to read chat messages

--- chat/src/App.js
+++ chat/src/App.js
@@ -4,14 +4,44 @@ import CreateMessage from './components/createMessage/createMessage';
 import ChatArea from './components/chatArea/chatArea';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <CreateMessage />
-      <ChatArea/>
-    </div>
-  );
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.host = 'st-chat.shas.tel';
+    this.uri = `ws://${this.host}`;
+    this.socket = new WebSocket(this.uri);
+    this.state = {
+      name: "",
+      message: "",
+      data: []
+    }
+  }
+  startSocket = () => {
+    if(typeof(WebSocket)=="undefined") { 
+        alert("Your browser does not support WebSockets. Try to use Chrome or Safari."); 
+    } else {
+        this.socket.onmessage = (event) =>  {
+          this.data = JSON.parse(event.data);
+          const arr = JSON.parse(event.data);
+          this.setState({data: arr});
+        }
+        this.socket.onerror = (error) => {
+            console.log('WebSocket Error: ' + error);
+        };
+        
+    }
+  }
+  
+  render() {
+    this.startSocket();
+    return (
+      <div className="App">
+        <Header/>
+        <CreateMessage socket={ this.socket }/>
+        <ChatArea messages={ this.state.data }/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- chat/src/components/chatArea/chatArea.jsx
+++ chat/src/components/chatArea/chatArea.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './chatArea.css';
 
-const ChatArea = () => {
+const ChatArea = (props) => {
     return (
         <div className="chat">
-            
+            { props.messages ? props.messages.map(message => {
+                    return (
+                            <div className="message" key={message.id}>
+                                <p>{`${new Date(message.time).toLocaleDateString()} ${new Date(message.time).toLocaleTimeString()}`} </p>
+                                <h3>{ message.from }</h3>
+                                <p>{ message.message }</p>
+                            </div>
+                            )
+                }) : <div className="message">No messages</div>
+            }
         </div>
     )
 }
